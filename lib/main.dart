f6a67a42a5d61f30c3f804c9f373546639a1e795
Awais Yousaf <awaisyousaf75@gmail.com>
2026-04-20 23:32:20 +0500

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_30days/pages/home_page.dart';
 import 'package:flutter_30days/pages/login_page.dart';
 import 'package:flutter_30days/utils/routes.dart';
 import 'package:flutter_30days/widgets/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
