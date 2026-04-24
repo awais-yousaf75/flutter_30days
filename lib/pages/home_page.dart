0af5533bd2a82f3ae5a04f710941f14b8d6042e4
Awais Yousaf <awaisyousaf75@gmail.com>
2026-04-24 23:14:52 +0500

    day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,10 +42,29 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: .antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: .circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(color: Colors.pinkAccent),
+                        child: Text(item.name),
+                      ),
+                      footer: Text("\$ ${item.price}"),
+                      child: Image.network(item.image),
+                    ),
+                  );
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) =>
-                    ItemWidget(item: CatalogModel.items[index]),
               )
             : Center(
                 child: CupertinoActivityIndicator(
